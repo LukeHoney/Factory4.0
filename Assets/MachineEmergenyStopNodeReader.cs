f3bde1eb0e27dcea4044edcab1d5d9ae74f4b6ed
LukeHoney <2248102@swansea.ac.uk>
2023-03-08 12:24:28 +0000

    Send Order Scripts Completed

---
+++ Assets/MachineEmergenyStopNodeReader.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using game4automation;
+using TMPro;
+
+public class MachineEmergenyStopNodeReader : MonoBehaviour
+{
+    [Header("Factory Machine")]
+    public string factoryMachineID;
+    public OPCUA_Interface Interface;
+
+
+    [Header("OPCUA Reader")]
+    public string nodeBeingMonitored;
+    public string nodeID;
+
+    //public TMP_Text digitalTwinFeedbackTMP;
+    public TMP_Text uiFeedbackTMP;
+    public string dataFromOPCUANode;
+
+    [Header("Outlines")]
+    public GameObject green;
+    public GameObject red;
+
+    void Start()
+    {
+        Interface.EventOnConnected.AddListener(OnInterfaceConnected);
+        Interface.EventOnConnected.AddListener(OnInterfaceDisconnected);
+        Interface.EventOnConnected.AddListener(OnInterfaceReconnect);
+    }
+
+
+    private void OnInterfaceConnected()
+    {
+        Debug.LogWarning("Connected to Factory Machine " + factoryMachineID);
+        var subscription = Interface.Subscribe(nodeID, NodeChanged);
+        dataFromOPCUANode = subscription.ToString();
+        Debug.LogError(dataFromOPCUANode);
+        //digitalTwinRFIDFeedbackTMP.text = RFIDInfo;
+        //uiRFIDFeedbackTMP.text = RFIDInfo;        
+    }
+
+    private void OnInterfaceDisconnected()
+    {
+        Debug.LogWarning("Factory Machine " + factoryMachineID + " has disconnected");
+    }
+
+    private void OnInterfaceReconnect()
+    {
+        Debug.LogWarning("Factory Machine " + factoryMachineID + " has reconnected");
+    }
+
+    public void NodeChanged(OPCUANodeSubscription sub, object value)
+    {
+        dataFromOPCUANode = value.ToString();
+        Debug.LogError("Factory machine " + factoryMachineID + " just registered " + nodeBeingMonitored + " as " + dataFromOPCUANode);
+    }
+
+
+    private void Update()
+    {
+        //uiFeedbackTMP.text = "Factory machine " + factoryMachineID + " just registered " + nodeBeingMonitored + " as " + dataFromOPCUANode;
+
+        if (dataFromOPCUANode == "False")
+        {
+            //badColour;
+            Debug.Log("Bad Colour ON");
+            green.SetActive(false);
+            red.SetActive(true);
+        }
+
+        else if (dataFromOPCUANode == "True")
+
+        {
+            //goodColour;
+            Debug.Log("Good Colour ON");
+            green.SetActive(true);
+            red.SetActive(false);
+        }
+
+        else
+        {
+            Debug.Log("No Colour On");
+        }
+    }
+}
